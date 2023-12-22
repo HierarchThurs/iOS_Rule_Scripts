0cdd6d56fe14ef869eab5e7dea7ee70f9b3d3416
Hierarch <70684678+HierarchThurs@users.noreply.github.com>
2023-12-22 09:52:43 +0800

    Add files via upload

---
+++ Scripts/12306.js
@@ -0,0 +1,192 @@
+/***********************************************
+
+> 应用名称：墨鱼自用12306去广告脚本
+> 使用说明：请在本地添加分流 host, ad.12306.cn, direct
+
+[rewrite_local]
+
+^https?:\/\/ad\.12306\.cn\/ad\/ser\/getAdList url script-analyze-echo-response https://github.com/ddgksf2013/Scripts/raw/master/12306.js
+
+[mitm]
+
+hostname = ad.12306.cn
+
+***********************************************/
+// 转换时间: 2023/12/22 09:27:53
+// 兼容性转换
+if (typeof $request !== 'undefined') {
+  const lowerCaseRequestHeaders = Object.fromEntries(
+    Object.entries($request.headers).map(([k, v]) => [k.toLowerCase(), v])
+  );
+
+  $request.headers = new Proxy(lowerCaseRequestHeaders, {
+    get: function (target, propKey, receiver) {
+      return Reflect.get(target, propKey.toLowerCase(), receiver);
+    },
+    set: function (target, propKey, value, receiver) {
+      return Reflect.set(target, propKey.toLowerCase(), value, receiver);
+    },
+  });
+}
+if (typeof $response !== 'undefined') {
+  const lowerCaseResponseHeaders = Object.fromEntries(
+    Object.entries($response.headers).map(([k, v]) => [k.toLowerCase(), v])
+  );
+
+  $response.headers = new Proxy(lowerCaseResponseHeaders, {
+    get: function (target, propKey, receiver) {
+      return Reflect.get(target, propKey.toLowerCase(), receiver);
+    },
+    set: function (target, propKey, value, receiver) {
+      return Reflect.set(target, propKey.toLowerCase(), value, receiver);
+    },
+  });
+}
+
+
+// QX 相关
+var setInterval = () => {}
+var clearInterval = () => {}
+var $task = {
+  fetch: url => {
+    return new Promise((resolve, reject) => {
+      if (url.method == 'POST') {
+        $httpClient.post(url, (error, response, data) => {
+          if (response) {
+            response.body = data
+            resolve(response, {
+              error: error,
+            })
+          } else {
+            resolve(null, {
+              error: error,
+            })
+          }
+        })
+      } else {
+        $httpClient.get(url, (error, response, data) => {
+          if (response) {
+            response.body = data
+            resolve(response, {
+              error: error,
+            })
+          } else {
+            resolve(null, {
+              error: error,
+            })
+          }
+        })
+      }
+    })
+  },
+}
+
+var $prefs = {
+  removeValueForKey: key => {
+    let result
+    try {
+      result = $persistentStore.write('', key)
+    } catch (e) {
+    }
+    try {
+      result = $persistentStore.write(null, key)
+    } catch (e) {
+    }
+    return result
+  },
+  valueForKey: key => {
+    return $persistentStore.read(key)
+  },
+  setValueForKey: (val, key) => {
+    return $persistentStore.write(val, key)
+  },
+}
+
+var $notify = (title = '', subt = '', desc = '', opts) => {
+  const toEnvOpts = (rawopts) => {
+    if (!rawopts) return rawopts 
+    if (typeof rawopts === 'string') {
+      if ('undefined' !== typeof $loon) return rawopts
+      else if('undefined' !== typeof $rocket) return rawopts
+      else return { url: rawopts }
+    } else if (typeof rawopts === 'object') {
+      if ('undefined' !== typeof $loon) {
+        let openUrl = rawopts.openUrl || rawopts.url || rawopts['open-url']
+        let mediaUrl = rawopts.mediaUrl || rawopts['media-url']
+        return { openUrl, mediaUrl }
+      } else {
+        let openUrl = rawopts.url || rawopts.openUrl || rawopts['open-url']
+        if('undefined' !== typeof $rocket) return openUrl
+        return { url: openUrl }
+      }
+    } else {
+      return undefined
+    }
+  }
+  console.log(title, subt, desc, toEnvOpts(opts))
+  $notification.post(title, subt, desc, toEnvOpts(opts))
+}
+var _scriptSonverterOriginalDone = $done
+var _scriptSonverterDone = (val = {}) => {
+  let result
+  if (
+    (typeof $request !== 'undefined' &&
+    typeof val === 'object' &&
+    typeof val.status !== 'undefined' &&
+    typeof val.headers !== 'undefined' &&
+    typeof val.body !== 'undefined') || true
+  ) {
+    try {
+      for (const part of val?.status?.split(' ')) {
+        const statusCode = parseInt(part, 10)
+        if (!isNaN(statusCode)) {
+          val.status = statusCode
+          break
+        }
+      }
+    } catch (e) {}
+    if (!val.status) {
+      val.status = 200
+    }
+    if (!val.headers) {
+      val.headers = {
+        'Content-Type': 'text/plain; charset=UTF-8',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST,GET,OPTIONS,PUT,DELETE',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+      }
+    }
+    result = { response: val }
+  } else {
+    result = val
+  }
+  console.log('$done')
+  try {
+    console.log(JSON.stringify(result))
+  } catch (e) {
+    console.log(result)
+  }
+  _scriptSonverterOriginalDone(result)
+}
+var window = globalThis
+window.$done = _scriptSonverterDone
+var global = globalThis
+global.$done = _scriptSonverterDone
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const version = 'V1.0.22';
+
+var obj=JSON.parse($request.body),ddgksf2013={};"0007"==obj.placementNo?(ddgksf2013.materialsList=[{billMaterialsId:"6491",filePath:"https://bing.img.run/uhd.php",creativeType:1}],ddgksf2013.advertParam={skipTime:1},ddgksf2013.code="00"):ddgksf2013="G0054"==obj.placementNo?{code:"00",materialsList:[{}]}:{code:"00",message:"无广告返回"},_scriptSonverterDone({body:JSON.stringify(ddgksf2013)});
